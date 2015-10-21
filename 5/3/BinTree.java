1144240e9207a32a2684194d3cb4604fb6a128b1
larmas <lucas.armas.7@gmail.com>
2015-10-21 10:41:41 -0300

    terminado

---
+++ 5/3/BinTree.java
@@ -0,0 +1,43 @@
+public class BinTree implements BinaryTreeBasic{
+	// raiz del arbol
+	private TreeNode root;
+	
+	//constructor arbol binario
+	public BinTree(){
+		root = null;
+	}
+
+	public Object getRoot(){
+		return root.getElem();
+	}
+
+	public void setRoot(Object node){
+		root = (TreeNode)node;              //nullpointer
+	}
+
+	public boolean isEmpty(){
+		return root == null;
+	}
+
+	public void makeEmpty(){
+		root = null;
+	}
+
+	public void printPreOrder(){
+		if (root!=null){
+			root.printPreOrderTreeNode();
+		}
+	}
+
+	public void printPostOrder(){
+		if (root!=null){
+			root.printPostOrderTreeNode();
+		}
+	}
+
+	public void printInOrder(){
+		if (root!=null){
+			root.printInOrderTreeNode();
+		}
+	}
+}
