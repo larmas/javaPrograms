26188a253fe27a06161ab4855a4c399c238f123a
larmas <lucas.armas.7@gmail.com>
2015-10-29 21:49:10 -0300

    correjir insert y delete

--- 5/7/Heap.java
+++ 5/7/Heap.java
@@ -12,25 +12,25 @@ public class Heap{
 	}/*end of constructor*/
 
 	/*metodo que intercambia dos valores en un arreglo*/
-	public void swap(int current, int p){
-		Comparable aux = array[current];
-		array[current] = array[p];
-		array[p] = aux;
+	public void swap(int x, int y){
+		Comparable aux = array[x];
+		array[x] = array[y];
+		array[y] = aux;
 	}/*end of method*/
 
 	/*devuelve el padre de un elemento*/
 	public int parent(int indexHijo){
-		return (indexHijo-1)/2;
+		return ((indexHijo-1)/2);
 	}/*end of method*/
 
 	/*devuelve el indice del hijo derecho*/
 	public int right(int indexParent){
-		return 2*(indexParent+1);
+		return (2*(indexParent+1));
 	}/*end of method*/
 
 	/*devuelve el indice del hijo izquierdo*/
 	public int left(int indexParent){
-		return (2*indexParent)+1;
+		return ((2*indexParent)+1);
 	}
 
 	/*metodo isEmpty devuelve true si el heap es vacio*/
@@ -39,6 +39,20 @@ public class Heap{
 	}/*end of method
 
 	/*inserta un elemento en el heap y lo ordena*/
+	/*metodo sort ordena el heap*/
+	public void sort (int index){
+		int indexMax = this.left(index);
+		int indexRight = this.right(index);
+		/*busco el maximo elemento de los hermanos*/
+		if (array[indexMax].compareTo(array[indexRight])<0){
+			indexMax = indexRight;
+		}
+		
+		if ((indexMax<=lenght) && array[index].compareTo(array[indexMax])>0){
+			this.swap(index,indexMax);
+			this.sort(indexMax);
+		}
+	}
 	public void insertHeap(Comparable item){
 		array[lenght]=item;
 		int current = lenght;
@@ -50,17 +64,14 @@ public class Heap{
 		lenght++;
 	}/*end of method*/
 
+
 	/*metodo deleteMax elimina el mayor elemento del heap(cabeza) y ordena*/
 	public Comparable deleteMax(){
-		swap(0,lenght-1);
 		Comparable deleted = array[lenght-1];
+		swap(0,lenght-1);
 		lenght--;
-		int current = lenght-1;
-
-		while (parent(current)>=0 && array[current].compareTo(array[parent(current)])>0){
-			swap(current,parent(current));
-			current = parent(current);
-		}
+		this.sort(0);
+		
 		return deleted;
 	}
 
